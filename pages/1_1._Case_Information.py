7f33b8ef03fea54e71705783755065be18b202e0
Ash Knipler <ash.knipler@gmail.com>
2026-08-31 18:29:21 +1000

    docs: add additional case information

--- pages/1_1._Case_Information.py
+++ pages/1_1._Case_Information.py
@@ -15,7 +15,13 @@ if not bool(st.session_state.get("user_identifier", "").strip()):
     st.stop()
 
 st.title("Case Information")
+st.markdown("""
 
+You are a student clinician working in the Melbourne Dental Clinic. You are seeing Daniel, a 31-year-old new patient, for a comprehensive oral health assessment.
+Before the appointment, you reviewed the available clinical information with your clinical supervisor. Your supervisor advised that a new set of bitewing radiographs is likely to be indicated as part of Daniel’s comprehensive assessment.
+Begin the consultation by introducing yourself and taking Daniel's medical, dental and social history.
+Communicate with Daniel as you would with a patient in clinical practice.
+""")
 # Create Computer display
 computer_screen_display("patient_medical_history")
 
